e4067103483a9c139a61a12312143dc5b1adf7d1
Arjith A R <122336800+Arjith8@users.noreply.github.com>
2026-06-07 14:05:56 +0530

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        length = len(nums) - 1
+        start = length
+        
+        while start >= 0:
+            if start + 3 <= length:
+                nums[start] += max(nums[start+3], nums[start+2])
+
+            elif start + 2 <= length:
+                nums[start] += nums[start+2]
+            
+            start -= 1
+
+        return max(nums[0:2])
