9e228c87bbff785a84971bb61325dd070b60ead0
Arjith A R <122336800+Arjith8@users.noreply.github.com>
2026-06-17 02:32:59 +0530

    Add: design-word-search-data-structure - submission-27

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-27.py
@@ -0,0 +1,37 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.source = {}
+        
+
+    def addWord(self, word: str) -> None:
+        current = self.source
+        for i in word:
+            current[i] = current.get(i, {})
+            current = current[i]
+        
+        current[None] = True            
+        
+
+    def search(self, word: str) -> bool:
+        stack = [self.source]
+        for i in word:
+            if not stack:
+                return False
+
+            temp = []
+            while stack:
+                current = stack.pop()
+                if i == '.':
+                    for j in current:
+                        if (j is not None):
+                            temp.append(current[j])
+                else:
+                    if i in current and i is not None:
+                        temp.append(current[i])
+            stack = temp
+
+        for i in stack:
+            if None in i:
+                return True
+        return False
