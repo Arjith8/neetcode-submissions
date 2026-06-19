af178235839fc161dde610a22289b9d16da370cc
Arjith A R <122336800+Arjith8@users.noreply.github.com>
2026-06-19 21:23:24 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # Few things to consider
+            # I dont want to jump to a index where jump == 0 unless its the end
+        
+        max_index_reachable = 0
+        for i in range(len(nums)-1):
+            index_reachable = i + nums[i]
+            max_index_reachable = max(max_index_reachable, index_reachable)
+            print(i, index_reachable, max_index_reachable)
+            if max_index_reachable <= i:
+                return False
+
+        return True
