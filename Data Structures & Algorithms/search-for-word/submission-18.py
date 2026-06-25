4f4f035b14d49a1f83ffc24cb624842d9cf99174
Arjith A R <122336800+Arjith8@users.noreply.github.com>
2026-06-25 22:40:13 +0530

    Add: search-for-word - submission-18

---
+++ Data Structures & Algorithms/search-for-word/submission-18.py
@@ -0,0 +1,25 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        resp = False
+        length = len(word)
+        def search(coords, current_index, track):
+            row, col = coords
+
+            if min(row, col) < 0 or row >= len(board) or col >= len(board[0]) or coords in track or board[row][col] != word[current_index]:
+                return False
+            
+            track.add(coords)
+            current_index += 1
+            if current_index == length:
+                return True
+            return search((row+1, col), current_index, track.copy()) or \
+                search((row-1, col), current_index, track.copy()) or \
+                search((row, col+1), current_index, track.copy()) or \
+                search((row, col-1), current_index, track.copy())
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                resp = resp or search((i, j), 0, set())
+
+        return resp
+        
