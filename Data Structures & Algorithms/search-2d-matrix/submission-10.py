31d93fa0e0f242959572c639a929db5d42ade692
Arjith A R <122336800+Arjith8@users.noreply.github.com>
2026-07-23 12:00:42 +0530

    Add: search-2d-matrix - submission-10

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-10.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        left, right = 0, len(matrix) - 1
+        contained_in = []
+        while left <= right:
+            current = left + (right-left)//2
+            if matrix[current][0] <= target and matrix[current][-1] >= target:
+                contained_in = matrix[current]
+                break
+            
+            if matrix[current][0] > target:
+                right = current - 1
+            else:
+                left = current + 1
+
+        if not contained_in:
+            return False
+            
+        left, right = 0, len(contained_in) - 1
+        while left <= right:
+            print(left, right)
+            current = left + (right-left)//2
+            print(current)
+            if contained_in[current] == target:
+                return True
+            
+            if contained_in[current] > target:
+                right = current - 1
+            else:
+                left = current + 1
+        return False
+        
+
+        
